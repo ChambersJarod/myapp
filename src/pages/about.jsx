524743e107fc14e65f6f236031e2e5c009c815c3
ChambersJarod <Jarod.Chambers-Genge@keyin.com>
2023-08-18 11:16:12 -0230

    made it searchable and added dummy database

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,84 @@
-import React from 'react'
-import Header from '../components/header';
-import Footer from '../components/footer';
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
-function About(){
-    return(
-        <div>
-            <Header/>
-            <h1>about</h1>
-            <Footer/>
+function About() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <header
+          style={{
+            backgroundColor: "#333",
+            color: "#fff",
+            textAlign: "center",
+            padding: "1em",
+          }}
+        >
+          <h1>About Us</h1>
+        </header>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{ maxWidth: "800px", padding: "2em", textAlign: "center" }}
+          >
+            <h2>Welcome to our Online Library!</h2>
+            <p>
+              We are passionate about books and knowledge. Our online library
+              provides a vast collection of books from various genres, catering
+              to readers of all ages and interests. Whether you're a fiction
+              enthusiast, a history buff, or someone looking for educational
+              resources, we have something for you.
+            </p>
+          </div>
         </div>
-    )
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{ maxWidth: "800px", padding: "2em", textAlign: "center" }}
+          >
+            <h3>Our Mission</h3>
+            <p>
+              Our mission is to make the joy of reading accessible to everyone.
+              We believe that books have the power to inspire, educate, and
+              entertain, and we strive to create an online platform that fosters
+              a love for reading and learning.
+            </p>
+          </div>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{ maxWidth: "800px", padding: "2em", textAlign: "center" }}
+          >
+            <h3>Our Team</h3>
+            <p>
+              Our dedicated team of librarians, developers, and book lovers work
+              tirelessly to curate and maintain our library's collection. We are
+              committed to providing a seamless and enjoyable reading experience
+              for our users.
+            </p>
+          </div>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{ maxWidth: "800px", padding: "2em", textAlign: "center" }}
+          >
+            <h3>Contact Us</h3>
+            <p>
+              If you have any questions, suggestions, or feedback, feel free to
+              reach out to us. We value your input and are always looking for
+              ways to improve our services.
+            </p>
+            <p>
+              Email:{" "}
+              <a href="mailto:info@secretpassage.com">info@secretpassage.com</a>
+              <br />
+              Phone: (123) 456-7890
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default About;
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,38 +1,79 @@
-import React from 'react'
-import Header from '../components/header';
-import Footer from '../components/footer';
+import React, { useState } from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
-function Search(){
-    if (window.sessionStorage.getItem("loggedin") == 'true'){
-        return(
-            <div>
-                <Header/>
-                    <div>
-                        <title>aviable books</title>
-                        <table>
-                            <tr>
-                                <th>Id</th>
-                                <th>Title</th>
-                                <th>Author</th>
-                                <th>Publisher</th>
-                                <th>Genre</th>
-                                <th>Publication date</th>
-                            </tr>
-                        </table>
-                    </div>
-                <Footer/>
-            </div>
-        )
-    }else{
-        return(
-            <div>
-                <Header/>
-                    
-                <Footer/>
-            </div>
-        )
-    }
+import "./search.css";
+
+function Search() {
+  const libraryData = [
+    { id: 1, title: "Book 1", author: "Author 1", genre: "Fiction" },
+    { id: 2, title: "Book 2", author: "Author 2", genre: "Mystery" },
+    { id: 3, title: "Book 3", author: "Author 3", genre: "Fantasy" },
+    { id: 4, title: "Book 4", author: "Author 4", genre: "Sci-Fi" },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredLibraryData = libraryData.filter(
+    (book) =>
+      book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      book.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      book.genre.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  if (window.sessionStorage.getItem("loggedin") == "true") {
+    return (
+      <div>
+        <Header />
+        <div>
+          <h2>Library Catalog</h2>
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search by title, author, or genre..."
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Genre</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredLibraryData.map((book) => (
+                <tr key={book.id}>
+                  <td>{book.id}</td>
+                  <td>{book.title}</td>
+                  <td>{book.author}</td>
+                  <td>{book.genre}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <Footer />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Header />
+        <div className="signinlink">
+          Please sign in to use our search function
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
-
 
 export default Search;
